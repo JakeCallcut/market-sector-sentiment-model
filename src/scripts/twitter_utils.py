5ea4230ccf230d3c612e9eeba67574a8a8eafc28
Jake Callcut <jakecallcut123@gmail.com>
2026-01-22 20:36:24 +0000

    completing finbert model and importing data

---
+++ src/scripts/twitter_utils.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+DEBUG = True
+IN_PATH = "../../data/twitter/raw/"
+OUT_PATH = "../../data/twitter/processed/"
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
