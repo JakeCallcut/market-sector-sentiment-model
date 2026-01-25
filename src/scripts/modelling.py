1766e2eaabe9c4a13197cb96ccbf1749e942da73
Jake Callcut <jakecallcut123@gmail.com>
2026-01-25 13:03:11 +0000

    aggregating sentiment scores

---
+++ src/scripts/modelling.py
@@ -0,0 +1,10 @@
+import pandas
+
+DEBUG = True
+RETURNS_PATH = "../../data/"
+SCORES_PATH = "../../data/twitter/processed/agg_scores/"
+
+def create_modelling_table():
+
+
+#create_modelling_table()
